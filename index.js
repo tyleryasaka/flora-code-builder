200cb64c6bd738cfe1bef21eceab7c93573654eb
Tyler Yasaka <contact@tyleryasaka.me>
2019-07-30 16:15:54 -0700

    Update index.js

--- index.js
+++ index.js
@@ -289,6 +289,9 @@ function genCode (items, colors, brightness, name) {
 
 Adafruit_NeoPixel pixels(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
 int analogPin = 9;
+int cR = 0;
+int cG = 0;
+int cB = 0;
 
 void setup() {
   Serial.begin(9600);
@@ -301,11 +304,48 @@ void loop() {
 ${loopCode}}
 
 void setColor(int r, int g, int b) {
-  pixels.setPixelColor(0, pixels.Color(r, g, b));
-  pixels.show();
+  while(cR != r || cG != g || cB != b) {
+    cR = adjustColor(cR, r);
+    cG = adjustColor(cG, g);
+    cB = adjustColor(cB, b);
+//    Serial.println(cR);
+//    Serial.println(cG);
+//    Serial.println(cB);
+    pixels.setPixelColor(0, pixels.Color(cR, cG, cB));
+    pixels.show();
+    delay(100);
+  }
+}
+
+int adjustColor(int current, int dest) {
+  if (current == dest) {
+    return dest;
+  }
+  int delta = dest - current;
+  int adjustment = (delta / 10);
+  if (adjustment == 0) {
+    if (delta > 0) {
+      adjustment = 5;
+    } else {
+      adjustment = -5;
+    }
+  }
+//  Serial.println("---");
+//  Serial.println(delta);
+//  Serial.println(adjustment);
+//  Serial.println("---");
+  int newVal = current + adjustment;
+  if (delta == 0) {
+    return dest;
+  } else if ((delta > 0 && newVal < dest) || (delta < 0 && newVal > dest)) {
+    return newVal;
+  } else {
+    return dest;
+  }
 }
 
 void lightOff() {
+  setColor(0, 0, 0);
   pixels.clear();
   pixels.show();
 }`
